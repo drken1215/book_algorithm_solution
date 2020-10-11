344d5a24dbe9e684c391932ec03b79a8987cebf9
Shinya Fujino <shf0811@gmail.com>
2020-10-12 01:04:30 +0900

    出力される文字列内における関数名を修正した

--- codes/chap04/code_4_6.cpp
+++ codes/chap04/code_4_6.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int fibo(int N) {
-    cout << "func(" << N << ") を呼び出しました" << endl;
+    cout << "fibo(" << N << ") を呼び出しました" << endl;
     
     // ベースケース
     if (N == 0) return 0;
