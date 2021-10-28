59d308f2ce894b2b01dbf08194f670fbf2b0f0ee
drken1215 <dr.ken.1215@gmail.com>
2021-10-28 12:35:18 +0900

    update errata

--- codes/chap06/code_6_5.cpp
+++ codes/chap06/code_6_5.cpp
@@ -2,17 +2,20 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-const long long INF = 1LL << 60; // 十分大きい値を 1 つ決める
 
 int main() {
     // 入力
     int N;
     cin >> N;
-    vector<long long> h(N), s(N);
-    for (int i = 0; i < N; i++) cin >> h[i] >> s[i];
+    vector<long long> H(N), S(N);
+    for (int i = 0; i < N; i++) cin >> H[i] >> S[i];
+
+    // 二分探索の上限値を求める
+    long long M = 0;
+    for (int i = 0; i < N; ++i) M = max(M, H[i] + S[i] * N);
 
     // 二分探索
-    long long left = 0, right = INF;
+    long long left = 0, right = M;
     while (right - left > 1) {
         long long mid = (left + right) / 2;
         
@@ -21,8 +24,8 @@ int main() {
         vector<long long> t(N, 0); // 各風船を割るまでの制限時間
         for (int i = 0; i < N; ++i) {
             // そもそも mid が初期高度より低かったらfalse
-            if (mid < h[i]) ok = false; 
-            else t[i] = (mid - h[i]) / s[i];
+            if (mid < H[i]) ok = false; 
+            else t[i] = (mid - H[i]) / S[i];
         }
         // 時間制限がさし迫っている順にソート
         sort(t.begin(), t.end()); 
